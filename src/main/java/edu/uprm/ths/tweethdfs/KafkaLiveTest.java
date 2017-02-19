32a063acf3bdeec52eeb07ae57b91cd1baa26e98
manuel <manuel.rodriguez7@upr.edu>
2017-02-19 15:08:33 -0400

    working of handler

--- src/main/java/edu/uprm/ths/tweethdfs/KafkaLiveTest.java
+++ src/main/java/edu/uprm/ths/tweethdfs/KafkaLiveTest.java
@@ -8,6 +8,7 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.Arrays;
 import java.util.Properties;
+import java.util.UUID;
 
 /**
  * Created by manuel on 2/19/17.
@@ -17,7 +18,8 @@ public class KafkaLiveTest {
     public static void main(String[] args) {
         Properties props = new Properties();
         props.put("bootstrap.servers", "node05.ece.uprm.edu:9092");
-        props.put("group.id", "tweets2");
+        //props.put("group.id", "tweets2");
+        props.put("group.id", "tweet-" + UUID.randomUUID().toString());
         props.put("enable.auto.commit", "true");
         props.put("auto.commit.interval.ms", "1000");
         props.put("session.timeout.ms", "30000");
@@ -26,14 +28,15 @@ public class KafkaLiveTest {
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         KafkaConsumer<String,String> consumer = new KafkaConsumer<String, String>(props);
         consumer.subscribe(Arrays.asList("trump"));
-        //Logger logger = LogManager.getLogger();
-        //logger.trace("Starting to get tweets");
+        Logger logger = LogManager.getRootLogger();
+        logger.trace("Starting to get tweets");
         for (int i=0; i < 20; ++i) {
-            //logger.trace("Iteration: " + i);
+            logger.trace("Iteration: " + i);
             ConsumerRecords<String, String> records = consumer.poll(100);
             for (ConsumerRecord<String, String> record : records) {
-                //logger.trace("records: " + records.count());
-                System.out.printf("offset = %d, key = %s, value = %s", record.offset(), record.key(), record.value());
+                logger.trace("records: " + records.count());
+                System.out.println(record.value());
+                //System.out.printf("offset = %d, key = %s, value = %s", record.offset(), record.key(), record.value());
             }
         }
         consumer.close();
