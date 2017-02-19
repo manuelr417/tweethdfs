95711d4b61b0622808ed0be1c5355ec7fe92b074
manuel <manuel.rodriguez7@upr.edu>
2017-02-19 12:17:58 -0400

    working of handler

---
+++ src/main/java/edu/uprm/ths/tweethdfs/KafkaLiveTest.java
@@ -0,0 +1,32 @@
+package edu.uprm.ths.tweethdfs;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.Arrays;
+import java.util.Properties;
+
+/**
+ * Created by manuel on 2/19/17.
+ */
+public class KafkaLiveTest {
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "node05.ece.uprm.edu:9092");
+        props.put("group.id", "tweets");
+        props.put("enable.auto.commit", "true");
+        props.put("auto.commit.interval.ms", "1000");
+        props.put("session.timeout.ms", "30000");
+        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        KafkaConsumer<String,String> consumer = new KafkaConsumer<String, String>(props);
+        consumer.subscribe(Arrays.asList("trump"));
+        for (int i=0; i < 20; ++i) {
+            ConsumerRecords<String, String> records = consumer.poll(100);
+            for (ConsumerRecord<String, String> record : records)
+                System.out.printf("offset = %d, key = %s, value = %s", record.offset(), record.key(), record.value());
+        }
+    }
+}
---
+++ src/main/java/edu/uprm/ths/tweethdfs/TweetKafkaConsumer.java
@@ -0,0 +1,11 @@
+package edu.uprm.ths.tweethdfs;
+
+/**
+ * Created by manuel on 2/19/17.
+ */
+public class TweetKafkaConsumer<S, S1> {
+
+    private HDFSHandler hdfsHandler;
+
+
+}
